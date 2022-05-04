195f611810ca359ee3107a259d9221db9c8b79db
antran1245 <antran1245@gmail.com>
2022-05-04 10:30:30 -0700

    completed pokemon api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([]);
+
+  const catchPokemon = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+    .then(response => response.json())
+    .then(data => setList(data.results))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={catchPokemon}>Fetch Pokemon</button>
+      <ul>
+        {list.map((item, i) => 
+          <li key={i}>{item.name}</li>
+        )}
+      </ul>
     </div>
   );
 }
